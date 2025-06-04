fb0407a925b8376bf1599935ce6004846bd218b0
WilliamSouza <wsouzaagostinho@gmail.com>
2025-06-04 14:23:05 -0300

    Inicio do projeto

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from './src/screens/LoginScreen';
+import ProductListScreen from './src/screens/ProductListScreen';
+import ProductDetailsScreen from './src/screens/ProductDetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Products" component={ProductListScreen} />
+        <Stack.Screen name="ProductDetails" component={ProductDetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
