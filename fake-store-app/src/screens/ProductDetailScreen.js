4c1c6f59c86767aa227112ffba4f52a65eb59503
William souza <86375281+WilliamAgostinho@users.noreply.github.com>
2025-06-04 14:11:30 -0300

    Delete fake-store-app directory

--- fake-store-app/src/screens/ProductDetailScreen.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function ProductDetailsScreen() {
-  return (
-    <View>
-      <Text>Detalhes do Produto</Text>
-    </View>
-  );
-}
--- fake-store-app/src/screens/ProductListScreen.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function ProductListScreen() {
-  return (
-    <View>
-      <Text>Lista de Produtos</Text>
-    </View>
-  );
-}
